13234cab2f6d0bafc4c13df504dad0b8d20bbf00
pasapples <pasapi@au1.ibm.com>
2015-06-17 19:52:55 +1000

    Initial commit

---
+++ src/main/java/demo/HelloController.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController
+{
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String sayHello ()
+    {
+        return "hello to you on " + new java.util.Date();
+    }
+}
